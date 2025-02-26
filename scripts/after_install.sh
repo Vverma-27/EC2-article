f65621b48e49811c45e9b38f3141251c15ad5763
Vihaan Verma <vverma270705@gmail.com>
2025-02-26 20:29:07 +0530

    fixed scripts

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,5 +2,9 @@
 
 #cd in working directory
 cd /home/ubuntu/my-express-app
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 #install dependencies
 npm i
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -6,11 +6,6 @@ sudo chmod -R 777 /home/ubuntu/my-express-app
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/my-express-app
 
-#add npm and node to path
-export NVM_DIR="$HOME/.nvm" 
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
-
 pm2 stop my-express-app
 #start our node app in the background
 pm2 start app.js --name my-express-app
