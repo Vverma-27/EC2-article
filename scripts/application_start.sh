f98a4b49375282c8efc195b7a01b8f45b51fdc28
Vihaan Verma <vverma270705@gmail.com>
2025-02-26 20:49:27 +0530

    added workflow

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,7 +5,9 @@ sudo chmod -R 777 /home/ubuntu/my-express-app
 
 #navigate into our working directory where we have all our github files
 cd /home/ubuntu/my-express-app
-npm i pm2 -g
+export NVM_DIR="$HOME/.nvm" 
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm 
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 pm2 stop my-express-app
 #start our node app in the background
